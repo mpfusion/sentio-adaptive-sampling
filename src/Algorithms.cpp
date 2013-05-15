1e7307bf296e052bf5aff492bd21b7a194209e17
Marco Patzer <homerow@lavabit.com>
2013-05-15 14:15:59 +0200

    Merge controller code and add algorithms code template

---
+++ src/Algorithms.cpp
@@ -0,0 +1,253 @@
+/*
+ * Algorithms.cpp
+ *
+ *  Created on: 2012-05-13
+ *      Author: Marco Patzer
+ */
+
+#include "Algorithms.h"
+
+#include <cmath>
+#include <cstdio>
+
+STATUS_BLOCK     Algorithms::myStatusBlock;
+INTERRUPT_CONFIG Algorithms::rtcInterruptConfig;
+
+Configuration Algorithms::config;
+
+time Algorithms::baseTime( 0 );
+time Algorithms::delayTime( 5 );
+
+uint8_t payload[11];
+
+volatile bool packetReceived  = false;
+volatile uint16_t packetCount = 0;
+
+Algorithms::Algorithms()
+{
+	myStatusBlock.numberOfISR         = 2;
+	myStatusBlock.restoreClockSetting = true;
+
+	rtcInterruptConfig.enableAlarm1           = true;
+	rtcInterruptConfig.enableBatteryBackedSQW = true;
+	rtcInterruptConfig.interruptControl       = true;
+
+	stateDefinition[initialState] = _initialState;
+	stateDefinition[mainstate]    = _mainstate;
+
+	ISR_Definition[0].function        = _ODD_GPIO_InterruptHandler;
+	ISR_Definition[0].interruptNumber = GPIO_ODD_IRQn;
+	ISR_Definition[0].anchorISR       = false;
+	ISR_Definition[1].function        = _EVEN_GPIO_InterruptHandler;
+	ISR_Definition[1].interruptNumber = GPIO_EVEN_IRQn;
+	ISR_Definition[1].anchorISR       = false;
+
+	config.sleep_time = 5;
+}
+
+
+bool Algorithms::_initialState()
+{
+#ifdef DEBUG
+	debug.initializeInterface( numberOfDebugPinsUsed, numberOfButtonsUsed );
+	debug.initializeDebugUart();
+
+	debug.printLine( "Algorithms initializing", true );
+#endif
+
+	timer.initializeInterface();
+	timer.setBaseTime( baseTime );
+	timer.setInterruptConfig( rtcInterruptConfig );
+	timer.initializeMCU_Interrupt();
+	timer.resetInterrupts();
+	timer.setLowPowerMode();
+
+	humid.initializeInterface();
+
+	luminance.initializeInterface();
+
+	GPIO_PinModeSet( enableTXS0102, gpioModePushPull, 1 );
+
+	confeh.setMode( DLC, DIRECT );
+	confeh.initializeInterface();
+
+	cc1101.initializeInterface();
+	cc1101.initializeRadioInterrupt0( false, true );
+	cc1101.setRfConfig();
+	cc1101.setAddress( _nodeID_algorithm );
+
+#ifdef DEBUG
+	debug.printLine( "\n", true );
+	debug.printLine( "Algorithms initialised", true );
+	sentio.LED_ClearRed();
+	sentio.LED_ClearOrange();
+	sentio.LED_ClearGreen();
+#endif
+
+	myStatusBlock.nextState = mainstate;
+
+	return true;
+}
+
+bool Algorithms::_mainstate()
+{
+#ifdef DEBUG
+	debug.printLine( "In mainstate", true );
+#endif
+
+	sendData( 42 );
+	receiveData();
+
+#ifdef DEBUG
+	debug.printLine( "going to sleep for ", false );
+	debug.printFloat( config.sleep_time, 0, true );
+#endif
+
+	baseTime  = 0;
+	timer.setBaseTime( baseTime );
+	timer.setAlarmPeriod( config.sleep_time, alarm1, alarmMatchHour_Minutes_Seconds );
+	timer.resetInterrupts();
+	timer.setLowPowerMode();
+
+	myStatusBlock.nextState   = mainstate;
+	myStatusBlock.sleepMode   = 3;
+	myStatusBlock.wantToSleep = true;
+
+	return true;
+}
+
+
+void Algorithms::sendData( float )
+{
+#ifdef DEBUG
+	debug.printLine( "Sending data start", true );
+#endif
+
+	uint8_t PAYLOAD[18];
+
+	PAYLOAD[0]  = 'H';
+	PAYLOAD[1]  = 'e';
+	PAYLOAD[2]  = 'l';
+	PAYLOAD[3]  = 'l';
+	PAYLOAD[4]  = 'o';
+	PAYLOAD[5]  = ' ';
+	PAYLOAD[6]  = 'M';
+	PAYLOAD[7]  = 'a';
+	PAYLOAD[8]  = 'u';
+	PAYLOAD[9]  = 'd';
+	PAYLOAD[10] = 'e';
+
+	cc1101.strobe( CC1101_SIDLE );
+	for ( volatile int i = 0; i < 4000; ++i );
+
+	const uint8_t packetType = 1;
+	cc1101.sendPacket( packetType, _nodeID_controller, PAYLOAD, sizeof( PAYLOAD ) );
+
+#ifdef DEBUG
+	debug.printLine( "Sending data finished", true );
+#endif
+
+}
+
+void Algorithms::receiveData()
+{
+
+#ifdef DEBUG
+	debug.printLine( "Receiving data start", true );
+#endif
+
+	cc1101.setReceiveMode();
+
+	debug.printLine( "Waiting for packet...", true );
+
+	do
+	{
+		while ( !packetReceived );
+		packetReceived = false;
+		debug.printLine( "Packet received", true );
+		sentio.LED_ToggleOrange();
+		cc1101.readPacket();
+		debug.printLine( "Node Address: ", false );
+		debug.printDecimal( cc1101.getAddress(), true );
+
+		debug.printLine( "Packet Address: ", false );
+		debug.printDecimal( cc1101.getPacketAddress() );
+
+		debug.printLine( "\n\rCRC Status: ", false );
+		debug.printDecimal( cc1101.getCrcStatus() );
+	}
+	
+	while ( !cc1101.getCrcStatus() || cc1101.getPacketAddress() != cc1101.getAddress() );
+
+	sentio.LED_ToggleGreen();
+	
+	debug.printLine( "Correct packet for this node received", true );
+
+	if ( packetCount < 65535 )
+		packetCount++;
+	else
+		packetCount = 1;
+
+	debug.printLine( "Node Address: ", false );
+	debug.printDecimal( cc1101.getAddress(), true );
+
+	debug.printLine( "Packet Address: ", false );
+	debug.printDecimal( cc1101.getPacketAddress() );
+	debug.printLine( " ", true );
+
+	cc1101.getPacketPayload( payload, 0, sizeof( payload ) - 1 );
+	/* debug.printLine( "Payload: ", false ); */
+	/* debug.printLine( reinterpret_cast<const char *>(payload), true ); */
+
+#ifdef DEBUG
+	debug.printLine( "Receiving data finished", true );
+#endif
+
+}
+
+
+ERROR_CODE Algorithms::executeApplication()
+{
+	return startApplication( &myStatusBlock );
+}
+
+
+uint8_t Algorithms::setupApplication()
+{
+	return initializeApplication( &myStatusBlock );
+}
+
+
+void Algorithms::_ODD_GPIO_InterruptHandler( uint32_t temp )
+{
+	if ( temp & maskInterruptRTC_wakeup )
+	{
+		switch ( myStatusBlock.nextState )
+		{
+		case initialState:
+			myStatusBlock.nextState = mainstate;
+			break;
+
+		case mainstate:
+			myStatusBlock.nextState = mainstate;
+			break;
+
+		default:
+			myStatusBlock.nextState = initialState;
+		}
+		
+		// Make sure the next state will be executed
+		myStatusBlock.wantToSleep = false;
+	}
+
+	GPIO_IntClear( ~0 );
+}
+
+void Algorithms::_EVEN_GPIO_InterruptHandler( uint32_t )
+{
+	packetReceived = true;
+	sentio.LED_ToggleRed();
+
+	// Clear the flag
+	GPIO_IntClear( ~0 );
+}
---
+++ src/Algorithms.h
@@ -0,0 +1,91 @@
+/*
+ * Algorithms.h
+ *
+ *  Created on: 2012-10-24
+ *      Author: Marco Patzer
+ */
+
+#ifndef ALGORITHMS_H_
+#define ALGORITHMS_H_
+
+#include "Statemachine.h"
+#include "DriverInterface.h"
+
+#include "time.h"
+#include "ApplicationConfig.h"
+#include "HistoricalAverage.h"
+
+
+enum ALGORITHMS
+{
+	initialState,
+	mainstate
+};
+
+struct Configuration
+{
+	int sleep_time;
+};
+
+
+/**
+ * Main controller class of the program.
+ *
+ * This is the main controller class of the application. It implements all
+ * necessary functionality and the states.
+ */
+class Algorithms : public Statemachine, public DriverInterface
+{
+private:
+
+	static STATUS_BLOCK myStatusBlock; ///< responsible for the state information
+
+	static Configuration config;
+
+	/**
+	 * Initialises the hardware.
+	 *
+	 * Initialises the used hardware components like timers and temperature
+	 * sensors. At the end of this function the orange LED is switched on.
+	 *
+	 * @return The return value is always `true,` since a return value of
+	 * `false` would stop this state machine.
+	 */
+	static bool _initialState();
+	
+	static bool _mainstate();
+
+	/**
+	 * Interrupt handler used for changing states after wakeup.
+	 *
+	 * @param This integer serves as a bit array and represents the GPIO ports
+	 * that can throw an interrupt. The exact port can be determined when a
+	 * bit mask is checked against this variable.
+	 */
+	static void _ODD_GPIO_InterruptHandler( uint32_t );
+	
+	static void _EVEN_GPIO_InterruptHandler( uint32_t );
+
+	static time baseTime;  ///< controls the starting value of the timer
+	static time delayTime; ///< controls the sleep duration
+
+	/**
+	 * Sends the data to a remote location.
+	 *
+	 * This function is currently not implemented.
+	 */
+	static void  sendData( float value );
+	
+	static void  receiveData();
+
+	static INTERRUPT_CONFIG rtcInterruptConfig;
+
+public:
+	Algorithms();
+	~Algorithms() {}
+
+	ERROR_CODE executeApplication();
+	uint8_t    setupApplication();
+};
+
+#endif /* ALGORITHMS_H_ */
